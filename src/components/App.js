ed564154a0bc3cb264f3ab0a401ee7cff6ea6cb2
Prabhakar Rai <student343.bhu@gmail.com>
2021-05-09 15:40:50 +0530

    reset

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Header from './Header';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <Header /> 
     </div>
   );
 }
